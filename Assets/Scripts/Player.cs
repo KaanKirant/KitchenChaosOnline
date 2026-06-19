9c79626ce4c6ef9d0642b19544f09b480dc72a6d
Kaan Kırant <kaankirant98@gmail.com>
2026-06-19 09:05:48 +0300

    Add Player controller and scene objects
    Add a new Player MonoBehaviour (Assets/Scripts/Player.cs) implementing basic WASD movement (moveSpeed = 7) and a Player GameObject with a Capsule child to the GameScene.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0); 
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized; // Normalize the vector to ensure consistent movement speed in all directions
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y); // Convert to 3D vector for movement
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Debug.Log(Time.deltaTime);
+    }
+}

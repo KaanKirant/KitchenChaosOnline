a883d1efd2598b49597d5da9cdc7a78eddf1f9e2
Kaan Kırant <kaankirant98@gmail.com>
2026-06-19 19:04:17 +0300

    Add ClearCounter prefab and player interaction
    Introduce a ClearCounter interactable: adds ClearCounter.cs and prefab  and places instances in GameScene. Update Player.cs to split Update into HandleMovement and HandleInteractions, track lastInteractDir, perform a raycast (2f) using a new countersLayerMask to detect and call ClearCounter.Interact(). Also update TagManager to add the "Counters" layer.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,42 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking() {  return isWalking; }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized(); // Get the normalized movement input from GameInput
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y); // Convert to 3D vector for movement
+
+        if(moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir; // Update the last interaction direction based on movement
+        }
+
+        float ineractDistance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, ineractDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized(); // Get the normalized movement input from GameInput
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y); // Convert to 3D vector for movement
@@ -56,6 +87,4 @@ public class Player : MonoBehaviour
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed); // Rotate the player to face the movement direction
     }
-
-    public bool IsWalking() {  return isWalking; }
 }

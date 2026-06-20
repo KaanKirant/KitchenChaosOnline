40a2ef07872cbf86db5da1a99efe156e1d5ba241
Kaan Kırant <kaankirant98@gmail.com>
2026-06-20 19:09:40 +0300

    Add KitchenObject SO, scripts and prefabs
    Introduce KitchenObjectSO and KitchenObject to represent spawnable kitchen items. Add CheeseBlock and Tomato prefabs and their ScriptableObject assets, plus folder .meta files. Move ClearCounter prefab into Counters and update it to reference a counterTopPoint and a kitchenObject (SO). Update ClearCounter.cs to instantiate the selected KitchenObjectSO prefab on Interact and log its name. Update GameScene prefab instances to reference the new SOs.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,9 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero; // Position the tomato at the center of the counter top
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }

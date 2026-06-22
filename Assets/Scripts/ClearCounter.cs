360406c9dd53d82d649ee53e72d9ef2557db2023
Kaan Kırant <kaankirant98@gmail.com>
2026-06-22 12:06:55 +0300

    Add kitchen object prefabs and container counters
    Add new KitchenObject prefabs (Bread, Cabbage, MeatPattyUncooked) and corresponding ContainerCounter prefabs for Bread, Cabbage, CheeseBlock, MeatPattyUncooked and Tomato. Add ScriptableObject assets for the new kitchen objects and their .meta files. Normalize transforms on existing CheeseBlock and Tomato prefabs (reset local positions/instance offsets and adjust child reference), and update GameScene.unity accordingly. Also include modifications to ClearCounter.cs and ContainerCounter.cs to support these additions.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,6 +7,30 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-
+        if (!HasKitchenObject())
+        {
+            //There is no KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //Player doesn't have a KitchenObject
+            }
+        }
+        else
+        {
+            //There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                //Player has a KitchenObject
+            }
+            else
+            {
+                //Player doesn't have a KitchenObject
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
